754563551bc3d8996929dc8a514dc2ad9dd0a3aa
Jack Danielski <jackdski555@gmail.com>
2017-07-11 01:01:15 -0600

    Update weather.py

--- weather.py
+++ weather.py
@@ -4,7 +4,8 @@ import json
 import requests
 from datetime import *
 
-import weatherIdAndWeek
+import declarations
+import funcs
 
 # ID for San Diego County: 5391832
 # ID for Poway: 5384690
@@ -16,147 +17,100 @@ data = requests.get(url)
 
 jsondata = json.loads(data.text)
 
-###   Functions   ###
-
-#finishes clothesString for clothing recommendation
-def clothes():
-	if avgTemp <= 40:
-		#clothesString = " wear pants   bootz  and  several  layers"
-		return " wear pants   bootz  and  several  layers"
-	elif avgTemp in range(41, 55):
-		return " wear pants and a sweater or jacket"
-	elif avgTemp in range(56, 65):
-		return " wear a sweater or jacket"
-	elif avgTemp in range(66, 72):
-		return " wear shorts or pants with a tee shirt"
-	elif avgTemp in range(73, 80):
-		return " wear  shrts   or   pants with a tee shirt"
-	elif avgTemp in range(81, 85 ):
-		return " wear shorts with a tee shirt"
-	else:
-		return " wear shorts    a tee shirt  and sandals  if appropriate"
-# write to string if it will rain in the early morning, morning, noon,
-# afternoon, or evening
-def precipTimes():
-	if whenItRains == [0,0,1,1,1]:
-		return "will continue from noon on"
-	elif whenItRains == [0,0,0,1,1]:
-		return "will start in the evening"
-	elif whenItRains == [1,1,0,0,0]:
-		return "only in the morning"
-	elif whenItRains == [0,1,1,1,0]:
-		return "only in the middle of the day"
-	elif whenItRains == [0,1,1,0,0]:
-		return "in the late morning"
-	elif whenItRains == [0,0,0,0,0]:
-		return "clear all day"
-	elif whenItRains == [1,1,1,1,1]:
-		return "all day"
-	elif whenItRains == [1,0,0,0,0]:
-		return "in the early morning"
-	elif whenItRains == [0,0,0,0,1]:
-		return "at night "
-	else :
-		return "scattered"
-
-
-
+weatherIcon = jsondata["list"][3]["weather"][0]["icon"]
+print weatherIcon
 
 # Date and time stuff
 
-currentDate = datetime.now().strftime("%y-%m-%d")
-str(currentDate)
-currentDate = "20" + currentDate + " 00:00:00"
+declarations.currentDate = datetime.now().strftime("%y-%m-%d")
+str(declarations.currentDate)
+declarations.currentDate = "20" + declarations.currentDate #+ " 00:00:00"
 print "Date:"
-print currentDate
+print declarations.currentDate
 
-dayOfWeek = int(datetime.today().strftime("%w")) + 1
+declarations.dayOfWeek = int(datetime.today().strftime("%w")) + 1
 print "Day of the Week:"
-print dayOfWeek
+print declarations.dayOfWeek
 
-day = ""
-for place in weatherIdAndWeek.week:
-	if dayOfWeek == place:
-		day = weatherIdAndWeek.week[place]
+for place in declarations.week:
+	if declarations.dayOfWeek == place:
+		declarations.day = declarations.week[place]
 print "Day:"
-print day
+print declarations.day
 
 # Weather Stuff
 
 # weather ID
-print "Weather ID:"
-print jsondata["list"][4]["weather"][0]["id"]
-
+#print jsondata["list"][4]["weather"][0]["id"]
 # At 8pm the third index in list is noon
+declarations.weatherID = jsondata["list"][3]["weather"][0]["id"]
+print "Weather ID:"
+print declarations.weatherID
 
-weatherID = jsondata["list"][3]["weather"][0]["id"]
-
-weatherType = ""
-for number in weatherIdAndWeek.weather:
-	if weatherID == number:
-		weatherType = weatherIdAndWeek.weather[number]
+for number in declarations.weather:
+	if declarations.weatherID == number:
+		declarations.weatherType = declarations.weather[number]
 
 #print jsondata["list"][3]
 
-	#precip stuff
-precipString = ""
+#precip stuff
 #loop through the day and see if it will rain
-whenItRains = [0,0,0,0,0]
-for number in range(0,6):
-	for id in range(200, 623):
-		if id == jsondata["list"][number]["weather"][0]["id"]:
-			whenItRains[number] = 1
-print "When It Will Rain:"
-print whenItRains
+declarations.whenItRains = [0,0,0,0,0]
+for number in range(0,5):
+	for value in range(200, 623):
+		if value == jsondata["list"][number]["weather"][0]["id"]:
+			declarations.whenItRains[number] = 1
 
-precipString = precipTimes()
+print "When It Will Rain:"
+print declarations.whenItRains
+
+
+declarations.precipString = funcs.precipTimes(declarations.whenItRains)
 #print precipString
 
 # Temperature Stuff
-maxTemp = int(jsondata["list"][0]["main"]["temp_max"])
-minTemp = int(jsondata["list"][0]["main"]["temp_max"])
+declarations.maxTemp = int(jsondata["list"][0]["main"]["temp_max"])
+declarations.minTemp = int(jsondata["list"][0]["main"]["temp_max"])
 
-	# go through first 6 indecies to get high and low temps
+# go through first 6 indecies to get high and low temps
 for number in range(0,6):
-	if int(jsondata["list"][number]["main"]["temp_max"]) > maxTemp:
-		maxTemp = int(jsondata["list"][number]["main"]["temp_max"])
+	if int(jsondata["list"][number]["main"]["temp_max"]) > declarations.maxTemp:
+		declarations.maxTemp = int(jsondata["list"][number]["main"]["temp_max"])
 
 for number in range(0,6):
-	if int(jsondata["list"][number]["main"]["temp_min"]) < minTemp:
-		minTemp = int(jsondata["list"][number]["main"]["temp_min"])
+	if int(jsondata["list"][number]["main"]["temp_min"]) < declarations.minTemp:
+		declarations.minTemp = int(jsondata["list"][number]["main"]["temp_min"])
 
 #maxTemp = str(jsondata["list"][4]["main"]["temp_max"]) #// 1
 #minTemp = str(jsondata["list"][4]["main"]["temp_min"]) #// 1
 
-avgTemp = (maxTemp + minTemp) / 2
+declarations.avgTemp = (declarations.maxTemp + declarations.minTemp) / 2
 
-maxTemp = str(maxTemp)
-minTemp = str(minTemp)
+declarations.maxTemp = str(declarations.maxTemp)
+declarations.minTemp = str(declarations.minTemp)
 
-
-maxTemp = maxTemp[:2]
-minTemp = minTemp[:2]
+declarations.maxTemp = declarations.maxTemp[:2]
+declarations.minTemp = declarations.minTemp[:2]
 
 print "Max Temp: "
-print maxTemp
+print declarations.maxTemp
 print "Min Temp: "
-print minTemp
+print declarations.minTemp
 
 # make clothes recommendations
 clothesString = "It might be  good idea to "
-clothesString = clothesString + clothes()
+clothesString = clothesString + funcs.clothes(declarations.avgTemp)
 
-#print clothesString
 
 # Write to shellscript file stuff
 
-weatherString = "Tomorrow  on " + day + "  the weather will  be   " + weatherType + "and " + precipString
-tempString = "The high  is  " + maxTemp + "   and  the  low    is   " + minTemp
+weatherString = "Tomorrow  on "+declarations.day+"  the weather will  be   "+declarations.weatherType+"  and " +declarations.precipString
+tempString = "The high  is  " + declarations.maxTemp + "   and  the  low    is   " + declarations.minTemp
 
 shellscript = open("talk.sh", "r+")
 #print "Name of shellscript: ", shellscript.name
-	#clear file
-shellscript.truncate()
+
+shellscript.truncate()	#clear file
 
 shellscript.write("#!/bin/bash\n")
 shellscript.write("echo \" " + weatherString + "\" | festival --tts\n")
