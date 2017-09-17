bfa1be9ceb7f3c594e96ebd4b9e12861eecfdc2c
Jack Danielski <jackdski555@gmail.com>
2017-09-16 18:34:43 -0600

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash/
+
+# setup.sh 
+# Author: Jack Danielski
+
+# Program that implements a daily task so that the Clothing Forecaster runs at 8pm
+
+# Important: switch path in cronfile.txt to where you saved main.sh
+
+
+echo "Add cronjob? This will run the Clothing Forecaster everyday at 8pm"
+echo "Y or N?"
+
+read answer
+#echo 
+
+
+if [ "$answer" == "Y" ] || [ "$answer" == "y" ]; then
+	crontab cronfile.txt
+	echo "Cron task added"
+elif [ "$answer" == "N" ] || [ "$answer" == "n" ]; then 
+	echo "Cron task NOT added"
+else 
+	echo "Error: Answer was not Y or N. Try again"
+fi
+
+
+echo "Install Festival Text-To-Speech? This enables the program to speak when being used"
+echo "Y or N?"
+
+read tts
+
+if [ "$tts" == "Y" ] || [ "$tts" == "y" ]; then
+	sudo apt-get install festival 
+	echo "Festival Text-To-Speech added"
+elif [ "$tts" == "N" ] || [ "$tts" == "n" ]; then 
+	echo "Festival Text-To-Speech NOT added"
+else 
+	echo "Error: Answer was not Y or N. Try again"
+fi
+
+
+
+exit
